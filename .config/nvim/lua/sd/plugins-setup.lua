ef8e813c2b1ba17129fff7769ae6cee8e52dff10
Alex Schmith <alexanderschmith@protonmail.com>
2024-02-27 14:35:03 -0500

    Update plugins-setup.lua

--- .config/nvim/lua/sd/plugins-setup.lua
+++ .config/nvim/lua/sd/plugins-setup.lua
@@ -29,7 +29,44 @@ end
 return packer.startup(function(use)
 	use("wbthomason/packer.nvim")
 
-	use("Mofiqul/dracula.nvim")
+	--use("Mofiqul/dracula.nvim")
+	use({
+    		'NTBBloodbath/doom-one.nvim',
+    		setup = function()
+        		-- Add color to cursor
+			vim.g.doom_one_cursor_coloring = false
+			-- Set :terminal colors
+			vim.g.doom_one_terminal_colors = true
+			-- Enable italic comments
+			vim.g.doom_one_italic_comments = false
+			-- Enable TS support
+			vim.g.doom_one_enable_treesitter = true
+			-- Color whole diagnostic text or only underline
+        	vim.g.doom_one_diagnostics_text_color = false
+			-- Enable transparent background
+			vim.g.doom_one_transparent_background = false
+
+        	-- Pumblend transparency
+			vim.g.doom_one_pumblend_enable = false
+			vim.g.doom_one_pumblend_transparency = 20
+
+        	-- Plugins integration
+			vim.g.doom_one_plugin_neorg = true
+			vim.g.doom_one_plugin_barbar = false
+			vim.g.doom_one_plugin_telescope = false
+			vim.g.doom_one_plugin_neogit = true
+			vim.g.doom_one_plugin_nvim_tree = true
+			vim.g.doom_one_plugin_dashboard = true
+			vim.g.doom_one_plugin_startify = true
+			vim.g.doom_one_plugin_whichkey = true
+			vim.g.doom_one_plugin_indent_blankline = true
+			vim.g.doom_one_plugin_vim_illuminate = true
+			vim.g.doom_one_plugin_lspsaga = false
+		end,
+		config = function()
+        	vim.cmd("colorscheme doom-one")
+   	 end,
+	})
 
 	use("nvim-lua/plenary.nvim")
 
@@ -57,6 +94,8 @@ return packer.startup(function(use)
 	use("L3MON4D3/LuaSnip")
 	use("saadparwaiz1/cmp_luasnip")
 	use("rafamadriz/friendly-snippets")
+
+	use("antonk52/bad-practices.nvim")
 
 	-- managing & installing lsp servers, linters & formatters
 	use("williamboman/mason.nvim") -- in charge of managing lsp servers, linters & formatters
@@ -87,6 +126,11 @@ return packer.startup(function(use)
 		end,
 	})
 
+	use {'nvim-orgmode/orgmode', config = function()
+  		require('orgmode').setup{}
+	end
+	}
+
 	-- auto closing
 	use("windwp/nvim-autopairs") -- autoclose parens, brackets, quotes, etc...
 	use({ "windwp/nvim-ts-autotag", after = "nvim-treesitter" }) -- autoclose tags
@@ -104,6 +148,8 @@ return packer.startup(function(use)
 	-- git integration
 	use("lewis6991/gitsigns.nvim") -- show line modifications on left hand side
 	use { 'NeogitOrg/neogit', requires = 'nvim-lua/plenary.nvim' }
+
+	use("folke/zen-mode.nvim")
 	
 	if packer_bootstrap then
 		require("packer").sync()
