8c1f82a608c1a7ac277e49a6cbb1356547ba7ad5
Alex Schmith <alexanderschmith@protonmail.com>
2024-05-07 09:59:04 -0400

    Update org.el

--- emacs/sd/org.el
+++ emacs/sd/org.el
@@ -1,5 +1,59 @@
 (load "~/.emacs.d/sd/packages.el")
 
+(use-package org
+  :straight t
+  :hook (org-mode . sd/org-mode-setup)
+  :config
+  (setq org-ellipsis " ▾"
+        org-hide-emphasis-markers t))
+
+(use-package org-bullets
+  :after org
+  :hook (org-mode . org-bullets-mode)
+  :custom
+  (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
+
+
+(require 'org-habit)
+(add-to-list 'org-modules 'org-habit)
+(setq org-habit-graph-column 60)
+
+;; Install org-present if needed
+(use-package org-present
+  :straight t)
+
+(use-package org-modern
+  :straight t)
+
+(with-eval-after-load 'org (global-org-modern-mode))
+
+
+(use-package org-brain
+  :straight t
+  :init
+  (setq org-brain-path "~/brain")
+  ;; For Evil users
+  (with-eval-after-load 'evil
+    (evil-set-initial-state 'org-brain-visualize-mode 'emacs))
+  :config
+  (bind-key "C-c b" 'org-brain-prefix-map org-mode-map)
+  (setq org-id-track-globally t)
+  (setq org-id-locations-file "~/.emacs.d/.org-id-locations")
+  (add-hook 'before-save-hook #'org-brain-straight-ids-in-buffer)
+  (push '("b" "Brain" plain (function org-brain-goto-end)
+          "* %i%?" :empty-lines 1)
+        org-capture-templates)
+  (setq org-brain-visualize-default-choices 'all)
+  (setq org-brain-title-max-length 12)
+  (setq org-brain-include-file-entries nil
+        org-brain-file-entries-use-title nil))
+
+;; Allows you to edit entries directly from org-brain-visualize
+(use-package polymode
+  :straight t
+  :config
+  (add-hook 'org-brain-visualize-mode-hook #'org-brain-polymode))
+
 ;; Replace list hyphen with dot
 (font-lock-add-keywords 'org-mode
                         '(("^ *\\([-]\\) "
@@ -26,8 +80,6 @@
 (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
 (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
 (set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch)
-
-
 
 
 
